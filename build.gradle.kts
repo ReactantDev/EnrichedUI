9f9debc73df3931c128cca6a000c4ad04a6a5104
Setako <zygdtako@outlook.com>
2020-06-02 21:57:45 +0800

    [Change] Update to reactant 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,15 @@ import java.net.URI
 
 val isSnapshot = true
 group = "dev.reactant"
-version = "0.1.7${if (isSnapshot) "-SNAPSHOT" else ""}"
+version = "0.2.0${if (isSnapshot) "-SNAPSHOT" else ""}"
 
-val kotlinVersion = "1.3.61"
+val kotlinVersion = "1.3.72"
 
 plugins {
     java
     `maven-publish`
     signing
-    kotlin("jvm") version "1.3.61"
+    kotlin("jvm") version "1.3.72"
     id("com.github.johnrengelman.shadow") version "5.0.0"
     id("org.jetbrains.dokka") version "0.10.0"
 }
@@ -38,11 +38,11 @@ repositories {
 dependencies {
     compileOnly(kotlin("stdlib-jdk8", kotlinVersion))
 
-    compileOnly("dev.reactant:reactant:0.1.7-SNAPSHOT")
-    compileOnly("dev.reactant:resource-stirrer:0.1.7-SNAPSHOT")
-    compileOnly("dev.reactant:mechanism:0.1.6-SNAPSHOT")
+    compileOnly("dev.reactant:reactant:0.2.0-SNAPSHOT")
+    compileOnly("dev.reactant:resource-stirrer:0.2.0-SNAPSHOT")
+//    compileOnly("dev.reactant:mechanism:0.1.6-SNAPSHOT")
 
-    compileOnly("org.spigotmc:spigot-api:1.15.1-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.15.2-R0.1-SNAPSHOT")
 }
 
 val dokka = (tasks["dokka"] as DokkaTask).apply {
